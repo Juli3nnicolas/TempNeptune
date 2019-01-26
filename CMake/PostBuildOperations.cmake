efd12c11a8009ad8238c7cb8d5346597adf97ed3
Julien Nicolas <ju.nicodev@gmail.com>
2019-01-26 19:16:16 +0100

    add post-build dll-copy

---
+++ CMake/PostBuildOperations.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required (VERSION 3.12)
+
+message("########## Running PostBuildOperations.cmake")
+
+# Copy shared libraries
+add_custom_command(TARGET Neptune POST_BUILD						# Adds a post-build event to Neptune
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different					# which executes "cmake - E copy_if_different..."
+        "${PROJECT_SOURCE_DIR}/Dependencies/Shadow/shadow.dll"      # <--this is in-file
+        $<TARGET_FILE_DIR:Neptune>)									# <--this is out-file path
