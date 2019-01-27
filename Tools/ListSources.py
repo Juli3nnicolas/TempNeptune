83c7290d1cf71b6b40858239145910f067908b8a
Julien Nicolas <ju.nicodev@gmail.com>
2019-01-28 00:22:36 +0100

    add a tool to list all files from a directory. it can append a custom path to the files

---
+++ Tools/ListSources.py
@@ -0,0 +1,38 @@
+#! /usr/local/bin/python3
+
+import os
+from sys import argv, exit
+
+if len(argv) < 2:
+    print(f"Error, you must provide a directory path to list the files from {len(argv)}")
+    exit(1)
+
+path = argv[1]
+prefix_path = argv[2]
+if "\\" in prefix_path:
+    prefix_path = prefix_path.replace("\\", "/")
+
+if prefix_path[-1] != "/":
+    prefix_path += "/" 
+
+# Don't know what exceptions are thrown by this function
+files = os.listdir(path)
+
+header_files = list()
+cpp_files = list()
+for f in files:
+    if f[-2:] == ".h":
+        header_files.append(prefix_path + f)
+    else:
+        if f[-4:] == ".cpp":
+            cpp_files.append(prefix_path + f)
+
+print("HEADER FILES:")
+for f in header_files:
+    print(f)
+
+print("\n\nCPP FILES:")
+for f in cpp_files:
+    print(f)
+
+print(f"\n\n.cpp + .h file count: {len(header_files) + len(cpp_files)}")
