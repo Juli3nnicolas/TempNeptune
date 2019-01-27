ccdfd01dc751ea4d73881a471ac90b340fb417eb
Julien Nicolas <ju.nicodev@gmail.com>
2019-01-27 13:35:52 +0100

    use platforms' name to include their libraries. Rename the directory Win32 to Windows

--- CMake/ImportDependencies.cmake
+++ CMake/ImportDependencies.cmake
@@ -5,26 +5,26 @@ cmake_minimum_required (VERSION 3.12)
 target_include_directories(Neptune PUBLIC "Dependencies/All/Include")
 
 # Add include directory for static and shared libs
-target_include_directories(Neptune PUBLIC "Dependencies/Win32/Include")
+target_include_directories(Neptune PUBLIC "Dependencies/${CMAKE_SYSTEM_NAME}/Include")
 
 #Link against static libraries
-find_library(LIBKTX_LIB "libktx.gl" "Dependencies/Win32/Static/X64/Release")
+find_library(LIBKTX_LIB "libktx.gl" "Dependencies/${CMAKE_SYSTEM_NAME}/Static/X64/Release")
 target_link_libraries(Neptune PUBLIC ${LIBKTX_LIB})
 
 # Link against loading libraries for DLLs
 
-find_library(ASSIMP_LIB "assimp" "Dependencies/Win32/Static/X64")
+find_library(ASSIMP_LIB "assimp" "Dependencies/${CMAKE_SYSTEM_NAME}/Static/X64")
 target_link_libraries(Neptune PUBLIC ${ASSIMP_LIB})
 
-find_library(GLEW32_LIB "glew32" "Dependencies/Win32/Static/X64")
+find_library(GLEW32_LIB "glew32" "Dependencies/${CMAKE_SYSTEM_NAME}/Static/X64")
 target_link_libraries(Neptune PUBLIC ${GLEW32_LIB})
 
-find_library(SDL2_LIB "SDL2" "Dependencies/Win32/Static/X64")
+find_library(SDL2_LIB "SDL2" "Dependencies/${CMAKE_SYSTEM_NAME}/Static/X64")
 target_link_libraries(Neptune PUBLIC ${SDL2_LIB})
 
 # Only useful if the target is an executable - remove when Neptune is made a static lib
 # Also, the SDL distribution should be updated
-find_library(SDL2_MAIN_LIB "SDL2main" "Dependencies/Win32/Static/X64")
+find_library(SDL2_MAIN_LIB "SDL2main" "Dependencies/${CMAKE_SYSTEM_NAME}/Static/X64")
 target_link_libraries(Neptune PUBLIC ${SDL2_MAIN_LIB})
 
 target_link_libraries(Neptune PUBLIC "legacy_stdio_definitions")
--- CMake/PostBuildOperations.cmake
+++ CMake/PostBuildOperations.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required (VERSION 3.12)
 message("########## Running PostBuildOperations.cmake")
 
 # Copy shared libraries
-add_custom_command(TARGET Neptune POST_BUILD						# Adds a post-build event to Neptune
-    COMMAND ${CMAKE_COMMAND} -E copy_directory						# which executes "cmake - E copy_if_different..."
-        "${PROJECT_SOURCE_DIR}/Dependencies/Win32/Shared/X64"      # <--this is in-file
-        $<TARGET_FILE_DIR:Neptune>)									# <--this is out-file path
+add_custom_command(TARGET Neptune POST_BUILD										# Adds a post-build event to Neptune
+    COMMAND ${CMAKE_COMMAND} -E copy_directory										# which executes "cmake - E copy_if_different..."
+        "${PROJECT_SOURCE_DIR}/Dependencies/${CMAKE_SYSTEM_NAME}/Shared/X64"		# <--this is in-file
+        $<TARGET_FILE_DIR:Neptune>)													# <--this is out-file path
