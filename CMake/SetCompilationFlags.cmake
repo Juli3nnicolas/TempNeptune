774ee8ffbf621144b1ebe0501da62475cb5b6fde
Julien Nicolas <ju.nicodev@gmail.com>
2019-05-03 04:16:14 +0200

    add compilation option tunning

---
+++ CMake/SetCompilationFlags.cmake
@@ -0,0 +1,20 @@
+# This script sets up the compilation flags for supported compilers
+
+cmake_minimum_required (VERSION 3.12)
+
+set(NEP_COMPILER_IS_SUPPORTED "False")
+
+if (MSVC)
+	set(NEP_COMPILER_IS_SUPPORTED "True")
+	set(NEP_DEBUG_OPTIONS "/MP")
+	set(NEP_RELEASE_OPTIONS "/MP")
+	target_compile_options(Neptune PUBLIC "$<$<CONFIG:DEBUG>:${NEP_DEBUG_OPTIONS}>")
+	target_compile_options(Neptune PUBLIC "$<$<CONFIG:RELEASE>:${NEP_RELEASE_OPTIONS}>")
+	#TODO: add the FINAL config
+endif()
+
+if (${NEP_COMPILER_IS_SUPPORTED} STREQUAL  "False")
+	# Log warning "The current compiler is not supported - the compilation won't be optimised"
+	message("The current compiler is not supported - the compilation won't be optimised")
+endif()
+
